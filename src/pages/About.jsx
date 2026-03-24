7c5000bba5c76424a19b1f4833a4b5e0e4939f32
edudanisilva08 <100476586+edudanisilva08@users.noreply.github.com>
2026-03-24 10:48:34 -0300

    Atualização Sobre
    Atualizei a descrição sobre minha pessoa.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
       <div className="hero-content">
         <h1>About</h1>
         <p className="subtitle">Saiba mais sobre mim</p>
-        <p>Sou um desenvolvedor web apaixonado por criar experiências digitais funcionais e com visuais agradáveis e inovadores.</p>
+        <p>Sou um desenvolvedor web apaixonado por criar experiências digitais funcionais e com visuais agradáveis e inovadores.<br /> Atualmente cursando ensino técnico com habilitação profissional em Desenvolvimento de Sistemas.</p>
       </div>
     </main>
   )
